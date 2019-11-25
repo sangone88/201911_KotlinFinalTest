709ed2bff6e0153c61b43315b014c8fe212d89ba
sangone88 <llil1@naver.com>
2019-11-25 21:03:20 +0900

    2등 제외 당첨 로직 작성

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
@@ -2,13 +2,14 @@ package com.tjoeun.a201911_kotlinfinaltest
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_lotto.*
 import java.util.*
 import kotlin.collections.ArrayList
 
 class LottoActivity : BaseActivity() {
 
-    var LottoNumArrayList = ArrayList<Int>()
+    var lottoNumArrayList = ArrayList<Int>()
     var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
     var myNumArrayList = ArrayList<Int>()
     var myTextViewArrayList = ArrayList<TextView>()
@@ -31,18 +32,43 @@ class LottoActivity : BaseActivity() {
 //    당첨 경과를 체크. 몇등인지 확인
     fun checkLottoRank() {
 
-//    6개 : 1등 => 20억원.
-//    5개 : 3등 => 150만원.
-//    4개 : 4등 => 5만원
-//    3개 : 5등 => 5천원
+//        6개 : 1등 => 20억원.
+//        5개 : 3등 => 150만원.
+//        4개 : 4등 => 5만원
+//        3개 : 5등 => 5천원
 //    그 이하 : 꽝 : 0원
+
+        var correctCount = 0
+        for (myNum in myNumArrayList) {
+            for (thisWeekNum in lottoNumArrayList) {
+                if (myNum == thisWeekNum) {
+                    correctCount++
+                }
+            }
+        }
+
+        if (correctCount == 6) {
+            Toast.makeText(mContext, "1등 당첨!", Toast.LENGTH_SHORT).show()
+        }
+        else if (correctCount == 5) {
+            Toast.makeText(mContext, "3등 당첨!", Toast.LENGTH_SHORT).show()
+        }
+        else if (correctCount == 4) {
+            Toast.makeText(mContext, "4등 당첨!", Toast.LENGTH_SHORT).show()
+        }
+        else if (correctCount == 3) {
+            Toast.makeText(mContext, "5등 당첨!", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(mContext, "꽝입니다.", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
     fun setThisWeekLottoNum() {
 
 //        당첨번호는 모두 날리고 다시 뽑자.
-        LottoNumArrayList.clear()
+        lottoNumArrayList.clear()
 
 //        6개의 텍스트뷰에 들어갈 당첨번호를 뽑아내는 반복문
         for (lottoNumTxt in thisWeekLottoNumTextViewArrayList) {
@@ -59,7 +85,7 @@ class LottoActivity : BaseActivity() {
 
 //                일단 중복 되지 않는다. (괜찮다)라고 전제하고 검사 시작
                 var isDuplOk = true
-                for (num in LottoNumArrayList) {
+                for (num in lottoNumArrayList) {
 //                    지금 만든 랜덤번호와 꺼내본 당첨번호가 같은가?
                     if (num == randomNum) {
 //                        중복되는 숫자를 발견!
@@ -71,7 +97,7 @@ class LottoActivity : BaseActivity() {
 //                중복검사를 통과했는지 확인
                 if (isDuplOk) {
 //                    만약 통과했다면 당첨번호롤 넣어주자.
-                    LottoNumArrayList.add(randomNum)
+                    lottoNumArrayList.add(randomNum)
 //                    올바른 번호를 뽑았으니 무한반복을 탈출.
                     break
                 }
@@ -80,12 +106,12 @@ class LottoActivity : BaseActivity() {
 //            lottoNumTxt.text = randomNum.toString()
         }
 //        당첨번호 6개를 작은 숫자부터 큰 숫자 순서대로 (정렬)!
-        Collections.sort(LottoNumArrayList)
+        Collections.sort(lottoNumArrayList)
 
 //        6개의 텍스트뷰 / 당첨번호를 뽑아내서 연결.
         for (i in 0..thisWeekLottoNumTextViewArrayList.size -1) {
             var numTxt = thisWeekLottoNumTextViewArrayList.get(i)
-            var number = LottoNumArrayList.get(i)
+            var number = lottoNumArrayList.get(i)
 
             numTxt.text = number.toString()
         }
