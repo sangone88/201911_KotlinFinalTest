ea2c879cd1f9c2a7a2333eef355ecd6d7d812dee
sangone88 <llil1@naver.com>
2019-11-26 20:42:54 +0900

    아이디 저장 여부 기록 기능 추가  - SharedPreference Setter / Getter 까지만

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a201911_kotlinfinaltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,14 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        saveIdCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+//                체크박스가 체크 되는 상황
+            }
+            else{
+//                해제되는 상황
+            }
+        }
 
     }
 
