c05ea3706a2f246fa060fc29bbfe898fa6d187e5
sangone88 <llil1@naver.com>
2019-11-26 21:19:35 +0900

    서버에 로그인 시도  - 응답처리는 아직 전혀 코딩 X

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.tjoeun.a201911_kotlinfinaltest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tjoeun.a201911_kotlinfinaltest.utils.ContextUtil
+import com.tjoeun.a201911_kotlinfinaltest.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,15 @@ class LoginActivity : BaseActivity() {
         loginBtn.setOnClickListener {
             val userId = idEdt.text.toString()
             val userPw = pwEdt.text.toString()
+
+            ServerUtil.postRequestLogin(mContext, userId, userPw, object : ServerUtil.JasonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+                }
+
+            })
         }
+
 
         saveIdCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
                 ContextUtil.setSaveIdChecked(mContext, isChecked)
