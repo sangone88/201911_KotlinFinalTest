52242d8d2c993f34a32058ee5ba91f43f81f2551
sangone88 <llil1@naver.com>
2019-11-27 20:13:06 +0900

    2개의 프래그먼트를 ViewPager에 실어서 표기.  - 단순히 배경색만 가진 프래그먼트 2개 생성

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/BoardActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/BoardActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.tjoeun.a201911_kotlinfinaltest.adapters.BoardViewPagerAdapter
 import com.tjoeun.a201911_kotlinfinaltest.utils.GlobalData
+import kotlinx.android.synthetic.main.activity_board.*
 
 class BoardActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,10 @@ class BoardActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        myViewPager.adapter = BoardViewPagerAdapter(supportFragmentManager)
+
+
+
 //        토스트로 로그인한 사람의 이름을 띄워주자.
         if (GlobalData.loginUserData != null) {
             Toast.makeText(mContext, GlobalData.loginUserData!!.name, Toast.LENGTH_SHORT).show()
