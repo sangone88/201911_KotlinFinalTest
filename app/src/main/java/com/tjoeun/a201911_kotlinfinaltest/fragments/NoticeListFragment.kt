e82da48e6fb73b6c84827adf71ca778e0b2b1e7f
sangone88 <llil1@naver.com>
2019-11-27 21:25:03 +0900

    리스트뷰 / 어댑터 연결

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/fragments/NoticeListFragment.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/fragments/NoticeListFragment.kt
@@ -7,13 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.tjoeun.a201911_kotlinfinaltest.R
+import com.tjoeun.a201911_kotlinfinaltest.adapters.NoticeAdapter
 import com.tjoeun.a201911_kotlinfinaltest.datas.Notice
 import com.tjoeun.a201911_kotlinfinaltest.utils.ServerUtil
+import kotlinx.android.synthetic.main.fragment_notice_list.*
 import org.json.JSONObject
 
 class NoticeListFragment : BaseFragment() {
 
     var noticeList = ArrayList<Notice>()
+    var noticeAdapter:NoticeAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +28,7 @@ class NoticeListFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        mContext
+        mContext = activity
         setupEvents()
         setValues()
     }
@@ -36,6 +39,9 @@ class NoticeListFragment : BaseFragment() {
     }
 
     override fun setValues() {
+        noticeAdapter = NoticeAdapter(mContext!!, noticeList)
+        noticeListView.adapter = noticeAdapter
+
         getNoticeFromServer()
     }
 
@@ -49,9 +55,11 @@ class NoticeListFragment : BaseFragment() {
                     val data = json.getJSONObject("data")
                     val notices = data.getJSONArray("notices")
 
+                    noticeList.clear()
                     for (i in 0..notices.length()-1) {
                         noticeList.add(Notice.getNoticeFromJson(notices.getJSONObject(i)))
                     }
+                    noticeAdapter?.notifyDataSetChanged()
                 }
                 else {
                     Toast.makeText(mContext!!, "서버 연결에 문제가 있습니다.", Toast.LENGTH_SHORT).show()
