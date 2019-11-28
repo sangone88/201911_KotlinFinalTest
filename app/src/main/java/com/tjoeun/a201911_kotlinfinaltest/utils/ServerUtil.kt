5e85a11d26132720f529ba09eb59f3db4786b6d5
sangone88 <llil1@naver.com>
2019-11-28 19:59:55 +0900

    게시판 게시글 등록하기

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/utils/ServerUtil.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/utils/ServerUtil.kt
@@ -51,6 +51,46 @@ class ServerUtil {
             })
         }
 
+
+        fun postRequestBlackList(context: Context, title:String, content:String, handler:JasonResponseHandler?) {
+
+            var client = OkHttpClient()
+            var url = "${BASE_URL}/black_list"
+
+//            POST/PUT/PATCH 메쏘드에서 요구하는 파라미터를 FormBody에 담아줌
+            var formBody = FormBody.Builder()
+                .add("title", title)
+                .add("content", content)
+                .build()
+
+//            실제로 날아갈 요청(request)을 생성.
+            var request = Request.Builder()
+                .url(url)
+                .header("X-Http-Token", ContextUtil.getUserToken(context))
+//                    post로 formBody를 가지고 감
+                .post(formBody)
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+//                    실패
+                    Log.e("서버통신에러", e.localizedMessage)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                    성공
+                    //                    Log.d("서버응답내용", body)
+                    var body = response.body()!!.string()
+//                    var json: JSONObject = JSONObject(body)  자료형 적을 필요도 없음
+                    var json = JSONObject(body)
+                    handler?.onResponse(json)
+
+                }
+
+            })
+        }
+
+
         fun getRequestNotice(context: Context, handler: JasonResponseHandler?) {
             var client = OkHttpClient()
             var urlBuilder = HttpUrl.parse("${BASE_URL}/notice")!!.newBuilder()
